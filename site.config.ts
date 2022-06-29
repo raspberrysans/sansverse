77502658c6544baabe488e20e6676fad3c9c1b9d
Sans Bhatia <sanz1475@yonsei.ac.kr>
2022-06-29 10:57:01 +0900

    2022 June portfolio

---
+++ site.config.ts
@@ -0,0 +1,64 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
+  rootNotionPageId: 'ec6f013522a4404f86bd4f806cd88648',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'sansverse by sans',
+  domain: 'sansverse.co',
+  author: 'Sans Bhatia',
+
+  // open graph metadata (optional)
+  description: 'sans personal website/portfolio',
+
+  // social usernames (optional)
+  // twitter: 'transitive_bs',
+  github: 'raspberrysans',
+  linkedin: 'sans-bhatia-41818a212',
+  newsletter: 'https://medium.com/@sansverse', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  pageUrlOverrides: {
+    '/blog': '84eebaf5abbb4c47b27649d3e916d66c',
+    // '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  },
+  // pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  navigationStyle: 'default'
+  // navigationStyle: 'custom',
+  // navigationLinks: [
+  //   {
+  //     title: 'About',
+  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+  //   },
+  //   {
+  //     title: 'Contact',
+  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //   }
+  // ]
+})
