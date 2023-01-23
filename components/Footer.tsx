eafa60e7de36118c4a8f3c1b9c646d18cb6eaea9
Sans Bhatia <sanz1475@yonsei.ac.kr>
2023-01-23 18:23:39 +0900

    [revised] copyright update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>Copyright 2023 {config.author}</div>
 
       <div className={styles.settings}>
         {hasMounted && (
@@ -44,7 +44,7 @@ export const FooterImpl: React.FC = () => {
             onClick={onToggleDarkMode}
             title='Toggle dark mode'
           >
-            {isDarkMode ?  <IoSunnyOutline /> : <IoMoonSharp />}
+            {isDarkMode ? <IoSunnyOutline /> : <IoMoonSharp />}
           </a>
         )}
       </div>
